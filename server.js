bb010cca19bae0936c36da84c829d9913105e105
raytickb10 <raytickb10@students.ecu.edu>
2019-03-21 10:41:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 // init project
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/APIAuthentication');
 
-var mongo = require('mongodb');
-
+const app = express(); 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-// http://expressjs.com/en/starter/basic-routing.html
+const {PORT, DATABASE_URL} = require('./config');
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -25,7 +22,47 @@ app.get('/login.html', function(request, response) {
   response.sendFile(__dirname + '/views/login.html');
 });
 
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+  console.log(databaseUrl);
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl,{ useNewUrlParser: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+};
+
 // listen for requests
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+/*var server = app.listen(3000, function () {
+  console.log("Listening on port %s", server.address().port);
+});*/
