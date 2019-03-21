bc1df85527ff24ca3c715cfcb75b447481070923
raytickb10 <raytickb10@students.ecu.edu>
2019-03-21 13:14:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    `mongodb://tester:poolshark7%@cluster0-tbga6.mongodb.net/test?retryWrites=true`;
+    `mongodb://tester:${encodeURIComponent('poolshark7%')}@cluster0-tbga6.mongodb.net/test?retryWrites=true`;
 exports.PORT = process.env.PORT || 8080;
