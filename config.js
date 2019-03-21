4ad595dbbe70173ff303ae1870b2660c02e317cd
raytickb10 <raytickb10@students.ecu.edu>
2019-03-21 10:42:52 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+exports.DATABASE_URL =
+    process.env.DATABASE_URL ||
+    global.DATABASE_URL ||
+    `mongodb+srv://tester:poolshark7%@cluster0-tbga6.mongodb.net/test?retryWrites=true`;
+exports.PORT = process.env.PORT || 8080;
