6e21f0ee61cb054f73205c184eb8e54f6c0e1571
raytickb10 <raytickb10@students.ecu.edu>
2019-06-03 15:30:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 'use strict';
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
-    `mongodb+srv://tester:${encodeURIComponent('poolshark7%')}@cluster0-tbga6.mongodb.net/test?retryWrites=true`;
+    global.DATABASE_URL ;
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
